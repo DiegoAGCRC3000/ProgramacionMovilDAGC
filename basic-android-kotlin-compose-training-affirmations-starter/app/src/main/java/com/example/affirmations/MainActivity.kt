50b6a23df40609aa4b8e2928dae20f94fa37c1bf
Diego Alberto Garcia Campuzano <s20120158@alumnos.itsur.edu.mx>
2023-09-20 22:16:42 -0600

    se creo una lista llamada AffirmationList de elemento composable, y se se mostro la lista con una expresion lambda

--- basic-android-kotlin-compose-training-affirmations-starter/app/src/main/java/com/example/affirmations/MainActivity.kt
+++ basic-android-kotlin-compose-training-affirmations-starter/app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -24,6 +24,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -31,11 +33,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.ModifierLocalReadScope
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.affirmations.data.Datasource
 import com.example.affirmations.model.Affirmation
 import com.example.affirmations.ui.theme.AffirmationsTheme
 
@@ -60,6 +64,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun AffirmationsApp() {
+    AffirmationList(
+        affirmationList = Datasource().loadAffirmations(),
+        )
 }
 @Composable
 fun AffirmationCard(affirmation: Affirmation, modifier : Modifier = Modifier){
@@ -85,4 +92,16 @@ fun AffirmationCard(affirmation: Affirmation, modifier : Modifier = Modifier){
 @Composable
 private fun AffirmationCardPreview(){
     AffirmationCard(Affirmation(R.string.affirmation1, R.drawable.image1))
+}
+
+@Composable
+fun AffirmationList(affirmationList:  List<Affirmation>, modifier: Modifier= Modifier){
+    LazyColumn(modifier= modifier){
+        items(affirmationList){affirmation ->
+            AffirmationCard(
+                affirmation = affirmation,
+                modifier = Modifier.padding(8.dp))
+        }
+    }
+
 }
